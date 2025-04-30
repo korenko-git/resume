888e213974e28cb9722fef0b39f7f81ee78deb0b
Dmytro Korenko <depiict@gmail.com>
2025-04-30 23:35:57 +0300

    style(ui): update link styling for consistent hover behavior
    Refactor the className properties for IconLink and Person components to ensure consistent hover background color across all links. This improves visual coherence and user experience.

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -17,7 +17,7 @@ export default function Person() {
 
       <Link
         href="public/cv-ats.pdf"
-        className="inline-flex transition-all border border-slate-300 dark:border-neutral-700 hover:border-opacity-0 border-opacity-50 shadow rounded-md bg-white/70 dark:bg-neutral-700/30 px-3 py-2.5 items-center text-slate-800 dark:text-white font-semibold group p-2 text-sm gap-x-2 mt-4 hover:bg-sky-500/25 mr-4"
+        className="inline-flex transition-all border border-slate-300 dark:border-neutral-700 hover:border-opacity-0 border-opacity-50 shadow rounded-md bg-white/70 dark:bg-neutral-700/30 px-3 py-2.5 items-center text-slate-800 dark:text-white font-semibold group p-2 text-sm gap-x-2 mt-4 hover:bg-slate-100 mr-4"
       >
         <span>ATS CV</span>
       </Link>
@@ -25,7 +25,7 @@ export default function Person() {
       {sectionData.email && (
         <Link
           href={`mailto:${sectionData.email}`}
-          className="inline-flex transition-all border border-slate-300 dark:border-neutral-700 hover:border-opacity-0 border-opacity-50 shadow rounded-md bg-white/70 dark:bg-neutral-700/30 px-3 py-2.5 items-center text-slate-800 dark:text-white font-semibold group p-2 text-sm gap-x-2 mt-4 hover:bg-sky-500/25"
+          className="inline-flex transition-all border border-slate-300 dark:border-neutral-700 hover:border-opacity-0 border-opacity-50 shadow rounded-md bg-white/70 dark:bg-neutral-700/30 px-3 py-2.5 items-center text-slate-800 dark:text-white font-semibold group p-2 text-sm gap-x-2 mt-4 hover:bg-slate-100"
         >
           <span>Email me</span>
           <span className="w-2 h-2 rounded-full bg-green-400 duration-1000 animate-pulse"></span>
