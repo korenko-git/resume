fd18f72437d1beb209fb0e2e400868e69bc8c770
Dmytro Korenko <depiict@gmail.com>
2025-05-05 01:07:11 +0300

    refactor(page.tsx): use getFirstPublishedEntry for metadata generation
    Refactor the metadata generation logic to use the getFirstPublishedEntry utility function for better maintainability and consistency. This ensures that the first published entry is used for metadata, improving the robustness of the code.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,18 @@ import type { Metadata } from "next";
 import HomeContent from "@/components/pages/HomeContent";
 
 import aboutData from "@/data/about.json";
+import { getFirstPublishedEntry } from "@/lib/entityUtils";
 
 export async function generateMetadata(): Promise<Metadata> {
-  if (!aboutData) {
+  const person = getFirstPublishedEntry(aboutData?.entries)
+  if (!aboutData || !person) {
     return {
       title: "Online Resume",
       description: "Professional resume with editing capabilities",
     };
   }
   
-  const { title, subtitle, description } = aboutData;
+  const { title, subtitle, description } = person;
   
   return {
     title: `${title} - ${subtitle}`,
