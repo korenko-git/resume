72341c4a7423c18aa9afd4a8ca3ccc85a329e1c1
Dmytro Korenko <depiict@gmail.com>
2025-05-06 16:00:41 +0300

    feat(Description): add LinkIcon to markdown links for better UX
    Enhance the user experience by adding a LinkIcon next to markdown links in the Description component. This provides a visual cue that the link will open in a new tab, improving accessibility and clarity.

--- src/components/resume/Entry/Description.tsx
+++ src/components/resume/Entry/Description.tsx
@@ -1,3 +1,4 @@
+import { LinkIcon } from "lucide-react";
 import ReactMarkdown from "react-markdown";
 
 interface DescriptionProps {
@@ -5,5 +6,25 @@ interface DescriptionProps {
 }
 
 export function Description({ data }: DescriptionProps) {
-  return <ReactMarkdown>{data.description}</ReactMarkdown>;
+  return (
+    <ReactMarkdown
+      components={{
+        a: ({ href, children, ...props }) => (
+          <a
+            className="relative mt-2 inline-flex items-center text-sm font-medium text-slate-300 hover:text-blue-300 focus-visible:text-blue-300"
+            href={href}
+            {...props}
+            target="_blank"
+            rel="noreferrer noopener"
+            aria-label={`${children} (opens in a new tab)`}
+          >
+            <LinkIcon className="w-4 h-4 mr-2"/>
+            <span>{children}</span>
+          </a>
+        ),
+      }}
+    >
+      {data.description}
+    </ReactMarkdown>
+  );
 }
