b929d0b6b91ea7de5d0f13bb1918d2200d8a4b25
Dmytro Korenko <depiict@gmail.com>
2025-05-08 14:43:12 +0300

    refactor(Skills): replace underscores and hyphens with spaces in skill names
    This change improves the readability of skill names by replacing underscores and hyphens with spaces, making them more user-friendly and consistent in the UI.

--- src/components/resume/Entry/Skills/index.tsx
+++ src/components/resume/Entry/Skills/index.tsx
@@ -12,7 +12,7 @@ export default function Skills({ data }: SkillsProps) {
       {data.skills.map((skill: string) => (
         <li className="mr-1.5 mt-2" key={skill}>
           <div className="flex items-center rounded-full border border-neutral-300 dark:border-neutral-700 border-opacity-70 shadow bg-neutral-100 dark:bg-neutral-400/10 px-3 py-1 text-xs font-medium leading-5 text-neutral-700 dark:text-neutral-300">
-            {skill}
+            {skill.replaceAll("_", " ").replaceAll("-", " ")}
           </div>
         </li>
       ))}
