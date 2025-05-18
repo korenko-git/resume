317edcaaaa9cc87b5b9489025ebac65899c04404
Dmytro Korenko <depiict@gmail.com>
2025-05-18 20:50:14 +0300

    refactor(ui): replace IconLink with Button component for consistency
    The IconLink component was removed and replaced with the Button component in SocialLinks and Person sections to maintain consistency across the UI. This change simplifies the codebase and ensures a unified approach to handling links and buttons.

--- src/components/resume/sections/Person.tsx
+++ src/components/resume/sections/Person.tsx
@@ -1,4 +1,4 @@
-import { IconLink } from "@/components/common/ui/IconLink";
+import { Button } from "@/components/common/ui/button";
 import { AboutEntry } from "@/types/resume";
 
 interface PersonProps {
@@ -19,26 +19,36 @@ export function Person({ sectionData }: PersonProps) {
         {sectionData.subtitle}
       </h2>
 
-      <ul className="flex items-center justify-center lg:justify-start">
-        <IconLink
-          href="cv-ats.pdf"
-          icon={<span>ATS CV</span>}
-          label="Download ATS CV"
-        />
+      <div className="mt-4 flex items-center justify-center lg:justify-start">
+        <Button
+          asChild
+          variant="outline"
+          className="mr-4 shrink-0 text-xs"
+          aria-label="Download ATS CV"
+        >
+          <a href={"/cv-ats.pdf"} target="_blank" rel="noopener noreferrer">
+            ATS CV
+          </a>
+        </Button>
 
         {sectionData.email && (
-          <IconLink
-            href={`mailto:${sectionData.email}`}
-            icon={
-              <>
-                <span>Email me</span>
-                <span className="w-2 h-2 rounded-full bg-green-400 duration-1000 animate-pulse"></span>
-              </>
-            }
-            label="Send email"
-          />
+          <Button
+            asChild
+            variant="outline"
+            className=" shrink-0 text-xs"
+            aria-label="Send email"
+          >
+            <a
+              href={`mailto:${sectionData.email}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span>Email me</span>
+              <span className="w-2 h-2 rounded-full bg-green-400 duration-1000 animate-pulse"></span>
+            </a>
+          </Button>
         )}
-      </ul>
+      </div>
     </>
   );
 }
