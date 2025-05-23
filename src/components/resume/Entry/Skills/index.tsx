84705b85f42f7e5259a9ca863fcdf3de724fdc66
Dmytro Korenko <depiict@gmail.com>
2025-05-23 19:06:20 +0300

    style(resume/Skills): add hover effect to skill text for better UX
    Enhance the user experience by adding a hover effect to skill text, making it more interactive and visually appealing on larger screens.

--- src/components/resume/Entry/Skills/index.tsx
+++ src/components/resume/Entry/Skills/index.tsx
@@ -11,7 +11,7 @@ export function Skills({ skills }: SkillsProps) {
     <ul className="mt-2 flex flex-wrap" aria-label="Technologies used">
       {skills.map((skill: string) => (
         <li className="mr-1.5 mt-2" key={skill}>
-          <div className="flex items-center rounded-full border border-border border-opacity-70 shadow bg-muted/30 px-3 py-1 text-xs font-medium leading-5 text-muted-foreground">
+          <div className="flex items-center rounded-full border border-border border-opacity-70 shadow bg-muted/30 px-3 py-1 text-xs font-medium leading-5 text-muted-foreground lg:group-hover:text-foreground">
             {skill}
           </div>
         </li>
