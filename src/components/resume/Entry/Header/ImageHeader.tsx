08ffdcf96469ac1fb2cba231eb8688e1c7dfe983
Dmytro Korenko <depiict@gmail.com>
2025-06-12 12:32:16 +0300

    refactor(ImageHeader): simplify image rendering logic with fallback

--- src/components/resume/Entry/Header/ImageHeader.tsx
+++ src/components/resume/Entry/Header/ImageHeader.tsx
@@ -8,23 +8,22 @@ interface ImageHeaderProps {
   image?: string;
 }
 
+const fallback = getAssetPath("/images/placeholder-logo.png");
 export function ImageHeader({ className, title, image }: ImageHeaderProps) {
   return (
     <header className={className} aria-label={title}>
-      {image && (
-        <img
-          src={getAssetPath(image)}
-          alt={title}
-          loading="lazy"
-          width="200"
-          height="48"
-          className="border-border/10 group-hover:border-border/30 aspect-video rounded border-2 object-cover transition sm:order-1 sm:col-span-2 sm:translate-y-1 lg:grayscale lg:group-hover:grayscale-0"
-          onError={(e) => {
-            e.currentTarget.src = getAssetPath("/images/placeholder-logo.png");
-            e.currentTarget.onerror = null;
-          }}
-        />
-      )}
+      <img
+        src={image ? getAssetPath(image) : fallback}
+        alt={title}
+        loading="lazy"
+        width="200"
+        height="48"
+        className="border-border/10 group-hover:border-border/30 aspect-video rounded border-2 object-cover transition sm:order-1 sm:col-span-2 sm:translate-y-1 lg:grayscale lg:group-hover:grayscale-0"
+        onError={(e) => {
+          e.currentTarget.src = fallback;
+          e.currentTarget.onerror = null;
+        }}
+      />
     </header>
   );
 }
